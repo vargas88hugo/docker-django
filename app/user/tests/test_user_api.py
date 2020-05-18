442f6d00a6667d2f039acf202710465594ba7af9
Hugo Vargas <vargas88hugo@gmail.com>
2020-05-17 19:19:06 -0500

    [Fix] Corrected style that affected python linter

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -100,4 +100,3 @@ class PublicUserApiTest(TestCase):
         res = self.client.post(TOKEN_URL, {'email': 'one', 'password': ''})
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-
